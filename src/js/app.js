b17dc75f521ddf06e37e39b07fb7aa048f6d5222
IsaGalo80 <isagalo1980@outlook.com>
2022-06-11 10:12:14 +0000

    conditional-profile-card

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,27 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name ? variables.name : "Escribe tu name"} ${
+    variables.lastname ? variables.lastname : "Escribe tu lastname"
+  }</h1>
+              <h2> ${variables.role ? variables.role : "Escribe tu rol"} </h2>
+          <h3>${variables.country ? variables.country : "Escribe tu country"} ${
+    variables.city ? variables.city : "Escribe tu city"
+  }</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/
+            ${variables.twitter ? variables.twitter : "Escribe tu twitter"}
+              
+            ><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.twitter ? variables.github : "Escribe tu github"
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.twitter ? variables.linkedin : "Escribe tu linkedin"
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.twitter ? variables.instagram : "Escribe tu twitter"
+            }"><i class="fa-brands fa-instagram fa-4"></i>"></a></li>
           </ul>
         </div>
     `;
